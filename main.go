c177ec697de9e69c42ef00eebfb96f548e534992
MihaiAshtifeni <75952158+MihaiAshtifeni@users.noreply.github.com>
2021-10-13 08:11:53 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,64 +1,21 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"sync/atomic"
-	"time"
-)
+import "os"
 
-type KitchenHandler struct{}
-
-var packetsReceived int32= 0
-var postReceived int32 = 0
-var latestOrder Order
-var latestOrderString string ="No order"
-
-func (KitchenHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	//fmt.Println("Received something")
-	atomic.StoreInt32(&packetsReceived, packetsReceived+1)
-	if r.Method == http.MethodPost {
-		atomic.StoreInt32(&postReceived, postReceived+1)
-		//TODO make buffer static
-		var buffer = make([]byte, r.ContentLength)
-		r.Body.Read(buffer)
-		var currentOrder Order
-		json.Unmarshal(buffer,&currentOrder)
-		if latestOrder.Id < currentOrder.Id {
-			latestOrder = currentOrder
-			latestOrderString = string(buffer)
-		}
-		//parseDiningHallRequest(buffer)
-		fmt.Fprintln(w, "Kitchen http request post method detected.")
-		fmt.Fprintln(w, "Kitchen request detected.\nPost Method Body:\n"+string(buffer))
-	} else {
-		fmt.Fprintln(w, "Kitchen server is UP on port "+kitchenServerPort)
-		fmt.Fprintf(w, "Recieved %d requests. Post requests: %d\n", packetsReceived, postReceived)
-		fmt.Fprintln(w,"Latest order:"+latestOrderString)
-	}
-}
-
-//func parseDiningHallRequest(buffer []byte) map[string]string {
-//	decoder := json.Decoder{}
-//	result := decoder.Decode(string(buffer))
-//
-//	//TODO return a order type
-//	return result
-//}
+var diningHallHost = "http://localhost"
 
 const diningHallPort = ":7500"
 const kitchenServerPort = ":8000"
+//todo fix multiple delivery problemd
+const cookN = 4
+const ovenN = 2
+const stoveN = 1
 
+var kitchen Kitchen
 func main() {
-	var kitchenServer http.Server
-	kitchenServer.Addr = kitchenServerPort
-	kitchenServer.Handler = KitchenHandler{}
-
-	fmt.Println(time.Now())
-	fmt.Println("Kitchen is listening and serving on port:" + kitchenServerPort)
-	if err := kitchenServer.ListenAndServe(); err != nil {
-		log.Fatal(err)
+	if args := os.Args; len(args) > 1{
+		//Set the docker internal host
+		diningHallHost = args[1]
 	}
+	kitchen.start()
 }
--- order.go
+++ order.go
@@ -1,11 +1,36 @@
 package main
 
 type Order struct {
-	Id      int `json:"id"`
-	TableId int `json:"table_id"`
-	WaiterId int   `json:"waiter_id"`
-	Items    []int `json:"items"`
-	Priority   int   `json:"priority"`
-	MaxWait    int   `json:"max_wait"`
-	PickUpTime int64 `json:"pick_up_time"`
+	id          int
+	tableId     int
+	items       []int
+	mealCounter int32
+	priority    int
+	pickUpTime  int64
+	maxWait     int
+	mealList    []*Meal
+}
+
+func parseOrder(postOrder *PostOrder) *Order {
+	ret := new(Order)
+	ret.id = postOrder.Id
+	ret.tableId = postOrder.TableId
+	ret.items = postOrder.Items
+	ret.mealCounter = 0
+	ret.priority = postOrder.Priority
+	ret.pickUpTime = postOrder.PickUpTime
+	ret.maxWait = postOrder.MaxWait
+	for _, id := range postOrder.Items {
+		ret.mealCounter += 1
+		meal := newMeal(ret, id)
+		ret.mealList = append(ret.mealList, meal)
+	}
+	return ret
+}
+
+func (order Order) isReady() bool {
+	if order.mealCounter != 0 {
+		return false
+	}
+	return true
 }
